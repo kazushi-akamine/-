7d5c24e2d65dee3a7f875b9a5b6d256430d4dd6d
parubro <paruobro@gmail.com>
2019-09-29 00:20:40 +0900

    Initial Commit

---
+++ tax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tax
+//
+//  Created by Maitoparta on 2019/09/29.
+//  Copyright © 2019 paruobro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
